1bf662eab124dc34b6aa67c1452c9d8b14e3529c
Dinesha Priyadarshani <dinesha_p@persistent.co.in>
2019-11-26 17:04:39 +0530

    create student register form

--- admin/src/app/app.module.ts
+++ admin/src/app/app.module.ts
@@ -1,22 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserRegisterComponent } from './pages/user-register/user-register.component';
 import { NavBarComponent } from './shared/components/nav-bar/nav-bar.component';
 import { MenuBarComponent } from './shared/components/menu-bar/menu-bar.component';
+import { HomeComponent } from './pages/home/home.component';
+
+const applicationRoutes:Routes = [
+  { path: 'dashboard', component: HomeComponent},
+  { path: 'register' , component: UserRegisterComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     MenuBarComponent,
-    UserRegisterComponent
+    UserRegisterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(applicationRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
